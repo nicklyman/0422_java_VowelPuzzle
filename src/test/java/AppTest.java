ff12c91732be4f2f0c0b7bf379828f1fe5bfa6b8
Nick Lyman <nick.lyman@gmail.com>
2016-04-22 15:47:54 -0700

    Incorporate fluentlenium integration test for results page to verify that it is being accessed correctly.  Test passes.

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -19,7 +19,7 @@ public class AppTest extends FluentTest {
   @Test
   public void rootTest() {
     goTo("http://localhost:4567/");
-    assertThat(pageSource()).contains("WORD PUZZLE!!!");
+    assertThat(pageSource()).contains("W-RD P-ZZL-!!!");
   }
   @Test
   public void containsVowels() {
@@ -27,5 +27,26 @@ public class AppTest extends FluentTest {
     fill("#userInput").with("tree");
     submit(".btn");
     assertThat(pageSource()).contains("tr--");
+  }
+  @Test
+  public void containsMultipleWordsAndVowels() {
+    goTo("http://localhost:4567/");
+    fill("#userInput").with("That is a tall tree!");
+    submit(".btn");
+    assertThat(pageSource()).contains("Th-t -s - t-ll tr--!");
+  }
+  @Test
+  public void containsMultipleWordsAndCapitalVowels() {
+    goTo("http://localhost:4567/");
+    fill("#userInput").with("THAT IS A TALL TREE!");
+    submit(".btn");
+    assertThat(pageSource()).contains("TH-T -S - T-LL TR--!");
+  }
+  @Test
+  public void resultsRootTest() {
+    goTo("http://localhost:4567/");
+    fill("#userInput").with("It is Friday!");
+    submit(".btn");
+    assertThat(pageSource()).contains("Can you figure the word puzzle out?!?");
   }
 }
